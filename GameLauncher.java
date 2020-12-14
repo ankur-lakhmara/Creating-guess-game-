e34b7e2ef6d4f869831ec523d66fde61ab74f078
Ankur Rohilla <59440628+ankur-lakhmara@users.noreply.github.com>
2020-12-14 15:46:17 +0530

    Create GameLauncher.java

---
+++ GameLauncher.java
@@ -0,0 +1,8 @@
+package Head_First_java.Chapter1;
+
+public class GameLauncher {
+    public static void main(String[]args){
+        GuessGame game = new GuessGame();
+        game.startGame();
+    }
+}
